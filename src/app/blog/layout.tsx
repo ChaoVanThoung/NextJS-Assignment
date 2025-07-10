7bec7af230f48413808a4a3eb8d775372707e680
Thoung168 <chaovanthoung168@gmail.com>
2025-07-10 16:36:52 +0700

    tes

--- src/app/blog/layout.tsx
+++ src/app/blog/layout.tsx
@@ -5,13 +5,39 @@ import StyledComponentsRegistry from "@/lib/registry"
 import BlogListSkeleton from "@/components/Skeleton/BlogSkeleton"
 import { Metadata } from "next"
 
-export const metadata:Metadata = {
-    title: "Blog Page",
-    description : "This is Blog Page where we share articles and update",
-    keywords:["blog","artickes","news","update","tech"],
-    authors:[{name: 'Full Stack Student'}],
-    applicationName:"Next.js Blog App"
-}
+export const metadata: Metadata = {
+  title: "Blog Page",
+  description: "This is Blog Page where we share articles and updates.",
+  keywords: ["blog", "articles", "news", "update", "tech"],
+  authors: [{ name: "Full Stack Student" }],
+  applicationName: "Next.js Blog App",
+
+  openGraph: {
+    title: "Blog Page - Next.js Blog App",
+    description: "Stay updated with our latest articles, tech news, and updates.",
+    siteName: "Next.js Blog App",
+    images: [
+      {
+        url: "https://mailrelay.com/wp-content/uploads/2018/03/que-es-un-blog-1.png",
+        width: 1200,
+        height: 630,
+        alt: "Blog banner image",
+      },
+    ],
+    locale: "en_US",
+    type: "website",
+  },
+
+  twitter: {
+    card: "summary_large_image",
+    title: "Blog Page - Next.js Blog App",
+    description: "Stay updated with our latest articles, tech news, and updates.",
+    creator: "@FullStackStudent", 
+    images: [
+      "https://mailrelay.com/wp-content/uploads/2018/03/que-es-un-blog-1.png",
+    ],
+  },
+};
 
 
 export default function BlogLayout(
--- src/app/product/layout.tsx
+++ src/app/product/layout.tsx
@@ -6,11 +6,33 @@ import { Metadata } from "next";
 
 export const metadata: Metadata = {
   title: "Product Page",
-  description: "this is Product Page showcasing available items",
+  description: "This is the Product Page showcasing available items.",
   keywords: ["product", "shop", "ecommerce", "buy"],
   authors: [{ name: "Full Stack Student" }],
   applicationName: "Next.js E-commerce Platform",
 
+  openGraph: {
+    title: "Product Page - Next.js E-commerce Platform",
+    description: "Explore the latest products available in our store.",
+    siteName: "Next.js E-commerce Platform",
+    images: [
+      {
+        url: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQkLiJTAyXQPJzo9uUstwm9bMQiQk6tHymLng&s",
+        width: 1200,
+        height: 630,
+        alt: "Product Preview",
+      },
+    ],
+    locale: "en_US",
+    type: "website",
+  },
+
+  twitter: {
+    card: "summary_large_image",
+    title: "Product Page - Next.js E-commerce Platform",
+    description: "Explore the latest products available in our store.",
+    images: ["https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQkLiJTAyXQPJzo9uUstwm9bMQiQk6tHymLng&s"],
+  },
 };
 
 export default function layout() {
